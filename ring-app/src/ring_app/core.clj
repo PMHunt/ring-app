bb79affb6e3a8acb241ead22d2c3566be0082c62
Phil Hunt <phil@deviantlogic.co.uk>
2021-05-03 17:11:53 +0100

    ring-http-response tested and working

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,12 +1,12 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]
+            [ring.util.http-response :as response]
             [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler
   "handles requests"
   [request-map]
-  (response/response
+  (response/ok
    (str "<html><body>Your IP is: "
         (:remote-addr request-map)
         "</body></html>")))
